5766cf8b3ccd1820d97d020eb06cf90a97b6799f
Tocknicsu <wingemerald@gmail.com>
2015-08-04 10:39:24 +0800

    update

--- backend/api/problems.py
+++
@@ -1,9 +0,0 @@
-from req import RequestHandler
-from req import reqenv
-from req import Service
-
-class ApiProblemsHandler(RequestHandler):
-    @reqenv
-    def get(self, page):
-        self.success({"page": page})
-        return
