90ac0a2efc22ab93791ff971202b1189e09489e3
Tocknicsu <wingemerald@gmail.com>
2015-09-09 21:47:13 +0800

    update

--- judgesystem/judgecenter/judgecenter.py
+++ judgesystem/judgecenter/judgecenter.py
@@ -7,7 +7,10 @@ class Judgecenter:
     def __init__(self):
         self.db = psycopg2.connect( host=config.db_host, dbname=config.db_dbname, user=config.db_user, password=config.db_password) 
 
+    def run(self):
+        while(1):
+            pass
 
 if __name__ == "__main__":
     judgecenter = Judgecenter()
-
+    judgecenter.run()
