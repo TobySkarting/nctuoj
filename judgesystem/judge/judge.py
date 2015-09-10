050ef1695f5f485903fc2da08d5ac6791fbae1f5
Tocknicsu <wingemerald@gmail.com>
2015-09-10 13:09:26 +0800

    update

--- judgesystem/judge/judge.py
+++ judgesystem/judge/judge.py
@@ -6,6 +6,9 @@ import ftp
 
 class Judge:
     def __init__(self):
-        self.s = socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.connect((config.judgecenter_host, config.judgecenter_port))
         pass
+
+if __name__ == "__main__":
+    judge = Judge()
