81eea1bfcfe56846e0f11cdd8d032b3a2354e08f
allenwhale <allenwhale@yahoo.com.tw>
2015-09-19 23:46:28 +0800

    update

--- backend/ftp.py
+++ backend/ftp.py
@@ -5,21 +5,25 @@ import datetime
 import sys
 import config
 import tornado
+from tornado.concurrent import run_on_executor
+from concurrent.futures import ThreadPoolExecutor
 
 class FTP:
+    executor = ThreadPoolExecutor(2)
     def __init__(self, server, port, user, password):
         self.server = server
         self.port = port
         self.user = user
         self.password = password
         self.createSSHClient()
-
+    
     def createSSHClient(self):
         self.client = paramiko.SSHClient()
         self.client.load_system_host_keys()
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.client.connect(self.server, self.port, self.user, self.password)
     
+    @run_on_executor
     def get(self, _from, _to):
         scp = SCPClient(self.client.get_transport())
         try: os.makedirs(os.path.split(_to)[0])
@@ -27,6 +31,7 @@ class FTP:
         scp.get(_from, _to)
         scp.close()
 
+    @run_on_executor
     def put(self, _from, _to):
         try: self.client.exec_command('mkdir -p %s' % os.path.split(_to)[0])
         except Exception as e: print(e)
