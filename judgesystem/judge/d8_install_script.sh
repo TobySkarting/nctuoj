ebafe55012b68d5c1b92457aa3509827b05b246c
Tocknicsu <wingemerald@gmail.com>
2015-09-25 18:46:53 +0800

    update

--- judgesystem/judge/d8_install_script.sh
+++ judgesystem/judge/d8_install_script.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 BUILD='build'
-sudo apt-get isntall libreadline6 libreadline6-dev 
-mkdir $BUILD
+#sudo apt-get install libreadline6 libreadline6-dev 
+#mkdir $BUILD
 cd $BUILD
-git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
-export PATH=$PATH:`pwd`/depot_tools
-echo $PATH
-gclient
-git clone git://github.com/v8/v8.git
-cd src && make dependencies && make x64.release library=shared soname_version=1.0 console=readline snapshot=off werror=no
-sudo cp out/x64.release/lib.target/lib* /usr/local/lib/
-sudo cp out/x64.release/d8 /usr/local/bin/
-sudo ln -sf /usr/local/lib/libv8.so.1.0 /usr/local/lib/libv8.so
-sudo mkdir /usr/local/include/v8
+#git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+#export PATH=$PATH:`pwd`/depot_tools
+#echo $PATH
+#gclient
+#git clone git://github.com/v8/v8.git
+cd v8
+#make dependencies
+#make x64.release library=shared soname_version=1.0 console=readline snapshot=off werror=no
+#sudo cp out/x64.release/lib.target/lib* /usr/local/lib/
+#sudo cp out/x64.release/d8 /usr/local/bin/
+#sudo ln -sf /usr/local/lib/libv8.so.1.0 /usr/local/lib/libv8.so
+#sudo mkdir /usr/local/include/v8
 sudo cp include/*.h /usr/local/include/v8/
