4319e536c25ba334f4878b8ddcfc629a2550b5e7
Tocknicsu <wingemerald@gmail.com>
2015-10-03 23:30:53 +0800

    update

--- backend/req.py
+++ backend/req.py
@@ -20,7 +20,7 @@ def md(s):
 class DatetimeEncoder(json.JSONEncoder):
     def default(self, obj):
         if isinstance(obj, datetime.datetime):
-            return obj.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
+            return obj.strftime('%Y-%m-%d %H:%M:%S')
         elif isinstance(obj, datetime.date):
             return obj.strftime('%Y-%m-%d')
         else:
