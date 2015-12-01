8485ef818fdee08dc188c3932a33b0a2fbc3161e
allenwhale <allenwhale@yahoo.com.tw>
2015-12-02 04:12:16 +0800

    update

--- backend/test.py
+++ backend/test.py
@@ -2,6 +2,7 @@ import subprocess
 import time
 child = subprocess.Popen(['python3', 'ftp.py', 'upload', 'pg.py', 'qq'])
 while child.poll() == None:
+
     print(child.returncode)
     time.sleep(0.01)
     print("--")
