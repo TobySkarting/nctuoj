48787286bc3b90fdc02ae69bd61428007b9e9815
allenwhale <allenwhale@yahoo.com.tw>
2015-12-16 20:59:07 +0800

    support pdf.js

--- backend/server.py
+++ backend/server.py
@@ -250,6 +250,7 @@ if __name__ == '__main__':
 
 
         ('/about/',                                                     WebAboutHandler),
+        ('/resource/(.*)', tornado.web.StaticFileHandler, {'path': '/mnt/nctuoj/data'}),
         ('/asset/(.*)', tornado.web.StaticFileHandler, {'path': '../http'}),
         ('/(google4e9e359eaf9accab.html)', tornado.web.StaticFileHandler, {'path': '../http'}),
         ('/.*',                                                         Web404Handler),
