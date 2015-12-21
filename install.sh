926836d516e826980e714bb7c609ff84bb93060a
Tocknicsu <wingemerald@gmail.com>
2015-12-22 03:01:02 +0800

    modify web config.py and fix install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,2 @@
-sudo apt-get install python3-pip python3 python3-dev redis-server
-sudo pip3 install tornado TorMySQL msgpack-python greenlet redis markdown2 
+sudo apt-get install python3-pip python3 python3-dev redis-server libpq-dev
+sudo pip3 install tornado TorMySQL msgpack-python greenlet redis markdown momoko 
