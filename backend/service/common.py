ab7d34a71a2ff5ecdd2f1bdb5b0e5d2457b2b964
allenwhale <allenwhale@yahoo.com.tw>
2016-02-01 15:27:37 +0800

    add redis to common.py

--- backend/service/common.py
+++ backend/service/common.py
@@ -8,9 +8,15 @@ class CommonService(BaseService):
         CommonService.inst = self
 
     def get_execute_type(self):
+        res = self.rs.get('execute_type')
+        if res: return res
         res ={ x['id']: x for x in (yield self.db.execute("SELECT * FROM execute_types order by id"))}
+        self.rs.set('execute_type', res)
         return res
 
     def get_verdict_type(self):
+        res = self.rs.get('verdict_type')
+        if res: return res
         res = { x['id']: x for x in (yield self.db.execute("SELECT * FROM map_verdict_string order by id"))}
+        self.rs.set('verdict_type', res)
         return res
