79aa113a3bf9c1d9443daa584a59b52eb59f1545
allenwhale <allenwhale@yahoo.com.tw>
2016-02-01 23:03:32 +0800

    add form_validation to service/school.py

--- backend/service/school.py
+++ backend/service/school.py
@@ -1,5 +1,6 @@
 from req import Service
 from service.base import BaseService
+from utils.form import form_validation
 
 class SchoolService(BaseService):
     def __init__(self, db, rs):
@@ -12,7 +13,12 @@ class SchoolService(BaseService):
 
     def get_school(self, data={}):
         required_args = ['id']
-        err = self.check_required_args(required_args, data)
+        required_args = [{
+            'name': '+id',
+            'type': int,
+        }]
+        # err = self.check_required_args(required_args, data)
+        err = form_validation(data, required_args)
         if err: return (err, None)
         res = yield self.db.execute('SELECT * FROM schools WHERE id=%s;', (data['id'],))
         if res.rowcount == 0:
