392ff5511d32459a31abd96ce4152b8d031d28d2
allenwhale <allenwhale@yahoo.com.tw>
2016-02-04 14:37:32 +0800

    remove redis from tag temporary

--- backend/service/tag.py
+++ backend/service/tag.py
@@ -10,12 +10,12 @@ class TagService(BaseService):
         TagService.inst = self
 
     def get_tag_list(self):
-        res = self.rs.get('tag_list')
-        if res: return (None, res)
+        # res = self.rs.get('tag_list')
+        # if res: return (None, res)
         res = yield self.db.execute('SELECT * FROM tags;')
         res = res.fetchall()
         print('RES: ',res)
-        self.rs.set('tag_list', res)
+        # self.rs.set('tag_list', res)
         return (None, res)
 
     def get_tag(self, data={}):
@@ -27,12 +27,12 @@ class TagService(BaseService):
         # err = self.check_required_args(required_args, data)
         err = form_validation(data, required_args)
         if err: return (err, None)
-        res = self.rs.get('tag@%s'%(str(data['id'])))
+        # res = self.rs.get('tag@%s'%(str(data['id'])))
         res = yield self.db.execute('SELECT * FROM tags WHERE id=%s;', (data['id'],))
         if res.rowcount == 0:
             return ('No tag ID', None)
         res = res.fetchone()
-        self.rs.set('tag@%s'%(str(data['id'])), res)
+        # self.rs.set('tag@%s'%(str(data['id'])), res)
         return (None, res)
 
     def post_tag(self, data={}):
@@ -53,7 +53,7 @@ class TagService(BaseService):
             id = id.fetchone()['id']
         else:
             id = data.pop('id')
-            self.rs.delete('tag@%s'%(id))
+            # self.rs.delete('tag@%s'%(id))
             sql ,param = self.gen_update_sql('tags', data)
             yield self.db.execute(sql, param)
         return (None, id)
@@ -67,7 +67,7 @@ class TagService(BaseService):
         # err = self.check_required_args(required_args, data)
         err = form_validation(data, required_args)
         if err: return (err, None)
-        self.rs.delete('tag@%s'%(str(data['id'])))
+        # self.rs.delete('tag@%s'%(str(data['id'])))
         yield self.db.execute('DELETE FROM tags WHERE id=%s', (data['id'],))
         return (None, None)
 
@@ -83,7 +83,7 @@ class TagService(BaseService):
         # err = self.check_required_args(required_args, data)
         err = form_validation(data, required_args)
         if err: return (err, None)
-        self.rs.delete('tag@problem@%s'%(data['problem_id']))
+        # self.rs.delete('tag@problem@%s'%(data['problem_id']))
         yield self.db.execute('INSERT INTO map_problem_tag (tag_id, problem_id) VALUES(%s, %s);', (data['tag_id'], data['problem_id']))
         return (None, None)
 
@@ -99,7 +99,7 @@ class TagService(BaseService):
         # err = self.check_required_args(required_args, data)
         err = form_validation(data, required_args)
         if err: return (err, None)
-        self.rs.delete('tag@problem@%s'%(data['problem_id']))
+        # self.rs.delete('tag@problem@%s'%(data['problem_id']))
         res = yield self.db.execute('DELETE FROM map_problem_tag WHERE tag_id=%s AND problem_id=%s RETURNING id;', (data['tag_id'], data['problem_id']))
         if res.rowcount == 0:
             return ('no this tag in this problem', None)
@@ -114,10 +114,10 @@ class TagService(BaseService):
         # err = self.check_required_args(required_args, data)
         err = form_validation(data, required_args)
         if err: return (err, None)
-        res = self.rs.get('tag@problem@%s'%(str(data['problem_id'])))
-        if res: return (None, res)
+        # res = self.rs.get('tag@problem@%s'%(str(data['problem_id'])))
+        # if res: return (None, res)
         res = yield self.db.execute('SELECT t.* FROM tags as t, map_problem_tag as m WHERE m.tag_id=t.id AND m.problem_id=%s;', (data['problem_id'],))
         res = res.fetchall()
         print('RES: ', res)
-        self.rs.set('tag@problem@%s'%(str(data['problem_id'])), res)
+        # self.rs.set('tag@problem@%s'%(str(data['problem_id'])), res)
         return (None, res)
