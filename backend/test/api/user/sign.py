473f2cf172baefa3dbfff62ea49f202fcdb54c79
Tocknicsu <wingemerald@gmail.com>
2016-02-20 01:47:45 +0800

    delete unused code

--- backend/test/api/user/sign.py
+++ backend/test/api/user/sign.py
@@ -23,31 +23,3 @@ class TestApiUserSign(TestCase):
         }
         self.assertEqualR(r, expect_result)
 
-# def user_login_test():
-
-    # ### try user login
-    # data = {
-        # "account": config.user_admin,
-        # "passwd": config.user_admin_password,
-    # }
-    # url = "%s/api/users/signin/"%(config.base_url,)
-    # result = requests.post(url, data=data)
-    # expect_result = {
-        # "status_code": 200,
-        # "text": {
-            # "msg": "",
-            # "status": 200,
-        # }
-    # }
-    # util.test("Normal User Login", url, data, expect_result)
-
-    # ### try error password
-    # data['account'] = "nothisuser"
-    # result = util.test("No Exist User Login", url, data)
-    # print(result.status_code, result.text)
-
-
-    
-
-# if __name__ == "__main__":
-    # user_login_test()
