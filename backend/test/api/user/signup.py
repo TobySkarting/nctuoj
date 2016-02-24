309846cf3c77dedfc1428170ab9056c483fb5e47
Tocknicsu <wingemerald@gmail.com>
2016-02-24 15:22:42 +0800

    test signup

--- backend/test/api/user/signup.py
+++ backend/test/api/user/signup.py
@@ -7,11 +7,25 @@ import datetime
 from util import TestCase
 import config
 
-class TestApiUserSignin(TestCase):
+class TestApiUserSignup(TestCase):
     url = "%s/api/users/signup/"%(config.base_url,)
 
     def test_user_signup(self):
         ### sign 'test'
+        ### miss email
+        data = {
+            "account": config.user_test_account,
+            "passwd": config.user_test_password,
+            "rpasswd": config.user_test_password,
+        }
+        res = requests.post(self.url, data=data)
+        expect_result = {
+            "status_code": 400,
+            "body": {
+                "msg": "",
+            }
+        }
+        self.assertEqualR(res, expect_result)
         ### sign 'test' again
         ### 'test' delete 'test'
         ### 'admin' delete 'test'
--- backend/test/main.py
+++ backend/test/main.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python3
 from api.user.signin import TestApiUserSignin
 from api.user.signup import TestApiUserSignup
 import unittest
