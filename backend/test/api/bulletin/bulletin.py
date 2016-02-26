07561bc29666e2dfe713b8fd7fb685e06b100644
Tocknicsu <wingemerald@gmail.com>
2016-02-26 22:47:55 +0800

    def some function

--- backend/test/api/bulletin/bulletin.py
+++ backend/test/api/bulletin/bulletin.py
@@ -8,4 +8,32 @@ from util import TestCase
 import config
 import common
 
-class TestApiUserSignin(TestCase):
+class TestApiBulletin(TestCase):
+    def __init__(self):
+        self.url = '/api/groups/1/bulletins/'%
+    def test_admin_post_bulletin(self):
+
+
+    def test_user_post_bulletin(self):
+        pass
+    def test_admin_put_bulletin(self):
+        pass
+    def test_user_put_bulletin(self):
+        pass
+    def test_admin_delete_bulletin(self):
+        pass
+    def test_user_delete_bulletin(self):
+        pass
+    def test_admin_post_another_bulletin(self):
+        pass
+    def test_user_post_another_bulletin(self):
+        pass
+    def test_admin_put_another_bulletin(self):
+        pass
+    def test_user_put_another_bulletin(self):
+        pass
+    def test_admin_delete_another_bulletin(self):
+        pass
+    def test_user_delete_another_bulletin(self):
+        pass
+
