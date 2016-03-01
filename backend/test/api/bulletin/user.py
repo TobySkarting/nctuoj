a79d9089342a1de5d5f7a2b11ab26ce197abe746
Tocknicsu <wingemerald@gmail.com>
2016-03-01 10:51:14 +0800

    complete test user bulletin

--- backend/test/api/bulletin/user.py
+++ backend/test/api/bulletin/user.py
@@ -14,7 +14,8 @@ class TestApiBulletinUser(TestCase):
     another_url = '%s/api/groups/2/bulletins/'%(config.base_url)
     another_urls = '%s/api/groups/2/bulletins/'%(config.base_url)
     user_token = common.get_user_info({'account': config.user_test_account, 'passwd': config.user_test_password})['token']
-
+    title = "Title test @ " + str(datetime.datetime.now())
+    content = "Content test @ " + str(datetime.datetime.now())
 
     def test_user_get_bulletins(self):
         data = {
@@ -27,18 +28,55 @@ class TestApiBulletinUser(TestCase):
     def test_user_post_bulletin(self):
         data = {
             "token": self.user_token,
-            "title": "user test post",
+            "title": self.title,
+            "content": self.content,
         }
+        res = requests.post(self.urls, data=data)
+        res.connection.close()
+        expect_result = {
+            "status_code": 403,
+            "body": {
+                "msg": "Permission Denied",
+            }
+        }
+        self.assertEqualR(res, expect_result)
 
-        pass
     def test_user_put_bulletin(self):
-        pass
+        data = {
+            "token": self.user_token,
+        }
+        res = requests.get(self.urls, data=data)
+        res.connection.close()
+        res = json.loads(res.text)['msg'][0]
+        data = {
+            "token": self.user_token,
+            "title": res['title'],
+            "content": "Modify @ " + str(datetime.datetime.now()) + res['content'],
+        }
+        res =requests.put( '%s%s/'%(self.url, res['id']), data=data)
+        res.connection.close()
+        expect_result = {
+            "status_code": 403,
+            "body": {
+                "msg": "Permission Denied",
+            }
+        }
+        self.assertEqualR(res, expect_result)
+
     def test_user_delete_bulletin(self):
-        pass
-    def test_user_post_another_bulletin(self):
-        pass
-    def test_user_put_another_bulletin(self):
-        pass
-    def test_user_delete_another_bulletin(self):
-        pass
+        data = {
+            "token": self.user_token,
+        }
+        res = requests.get(self.urls, data=data)
+        res.connection.close()
+        res = json.loads(res.text)['msg'][0]
+        res = requests.delete( '%s%s/'%(self.url, res['id']), data=data)
+        res.connection.close()
+        expect_result = {
+            "status_code": 403,
+            "body": {
+                "msg": "Permission Denied",
+            }
+        }
+        self.assertEqualR(res, expect_result)
 
