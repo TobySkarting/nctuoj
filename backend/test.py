05a49b9033a300659696859154e2da9ebcb88de5
Tocknicsu <wingemerald@gmail.com>
2016-03-02 09:18:44 +0800

    remove unused file

--- backend/test.py
+++
@@ -1,8 +0,0 @@
-import subprocess
-import time
-child = subprocess.Popen(['python3', 'ftp.py', 'upload', 'pg.py', 'qq'])
-while child.poll() == None:
-    print(child.returncode)
-    time.sleep(0.01)
-    print("--")
-    
