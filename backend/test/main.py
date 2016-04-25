f82765f2663af4fc9706665e702c2181f2b44596
Tocknicsu <wingemerald@gmail.com>
2016-04-25 21:27:33 +0800

    update

--- backend/test/main.py
+++ backend/test/main.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python3
-#from api.user.signin import TestApiUserSignin
-#from api.user.signup import TestApiUserSignup
+from api.user.signin import TestApiUserSignin
+from api.user.signup import TestApiUserSignup
 from api.bulletin.admin import TestApiBulletinAdmin
-#from api.bulletin.admin_cross import TestApiBulletinAdminCross
-#from api.bulletin.user import TestApiBulletinUser
-#from api.bulletin.guest import TestApiBulletinGuest
-#from api.problem.admin import TestApiProblemAdmin
-#from api.problem.user import TestApiProblemUser
-#from api.problem.guest import TestApiProblemGuest
+from api.bulletin.admin_cross import TestApiBulletinAdminCross
+from api.bulletin.user import TestApiBulletinUser
+from api.bulletin.guest import TestApiBulletinGuest
+from api.problem.admin import TestApiProblemAdmin
+from api.problem.user import TestApiProblemUser
+from api.problem.guest import TestApiProblemGuest
 from util import *
 import unittest
 if __name__ == '__main__':
