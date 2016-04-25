4a73af7f87d661709439a892c02047eda19e2c82
allenwhale <allenwhale@yahoo.com.tw>
2016-04-25 21:29:53 +0800

    remove unused code

--- backend/permission/api/submission.py
+++ backend/permission/api/submission.py
@@ -5,7 +5,6 @@ from map import *
 class ApiSubmissionsPermission(PermissionBase):
     def post(req, data):
         err, res = yield from Service.Problem.get_problem({'id': data['problem_id']})
-        print(res)
         if err: return err
         if not (req.current_group == res['group_id'] and \
                 (map_group_power['submission_manage'] in req.current_group_power or res['visible'] > 0)):
